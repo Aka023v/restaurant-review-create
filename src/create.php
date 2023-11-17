1ee1af366c4f5789e089e9eff9ee88835318b9c0
Aka023v <148590386+Aka023v@users.noreply.github.com>
2023-11-17 18:43:10 +0900

    作成

--- src/create.php
+++ src/create.php
@@ -1,23 +1,2 @@
-<body> 
-    <h1>新規作成</h1>
-    <form method="post" action="./store.php">
-        <table>
-            <tr>
-                <td>投稿者名</td>
-                <td><input name="poster" placeholder="投稿者名" required></td>
-            </tr>
-            <tr>
-                <td>レストラン名</td>
-                <td><input name="restaurant_name" placeholder="レストラン名" required></td>
-            </tr>
-            <tr>
-                <td>レビュー</td>
-                <td><textarea name="review" placeholder="レビュー記入欄" required></textarea></td>
-            </tr>
-            <tr>
-                <td ></td>
-                <td><button>送信</button></td>
-            </tr>
-        </table>
-    </form>
-</body>
+<?php
+echo "作成機能を作成しよう！";
